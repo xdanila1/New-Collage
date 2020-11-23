542d68e789ef6a51b2da16877a3de346363025f3
MrSav7 <mrsavarenkov@gmail.com>
2020-11-24 00:18:45 +0300

    Добавил ускорение на leftShift,  камеру от первого лица(она выключена) и камеру для разработчиков DEVCamera

--- Assets/scripts/move.cs
+++ Assets/scripts/move.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
-
+    public float xShift;
     public float speed;
     public float gravity;
     private CharacterController Character;
     void Start()
     {
-        speed = 1;
+        xShift = 1.5f;
+        speed = 3;
         gravity = -9.8f;
         
     }
@@ -21,9 +22,21 @@ public class move : MonoBehaviour
         if (Character == null) Debug.Log("Не обнаружен контроллер");
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
-        Vector3 move = new Vector3(x, gravity,y)*Time.deltaTime * speed;
-        move = transform.TransformDirection(move);
-        Character.Move(move);
+        
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Vector3 move = new Vector3(x, gravity, y) * Time.deltaTime * (speed*xShift);
+            move = transform.TransformDirection(move);
+            Character.Move(move);
+        }
+        else
+        {
+            Vector3 move = new Vector3(x, gravity, y) * Time.deltaTime * speed;
+            move = transform.TransformDirection(move);
+            Character.Move(move);
+        }
+        
+       
         
     }
 }
